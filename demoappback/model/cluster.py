16e5a8c64c455b15fc79bd9393c211c116f40e4d
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-05-24 14:43:48 -0400

    VER3-299 update cluster

--- demoappback/model/cluster.py
+++ demoappback/model/cluster.py
@@ -6,6 +6,6 @@ class Cluster(IsuFactor):
     Class describing outcomes.
     """
 
-    def __init__(self, name: str = None, standard_deviation = 1):
+    def __init__(self, name: str = None, cluster_level: int = 0):
         super.__init__(self, name, IsuFactorNature.WITHIN, IsuFactorType.CLUSTER)
-        self.standard_deviation = standard_deviation
+        self.cluster_level = cluster_level
