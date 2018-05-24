8f3b739f8b795fdc241161c618e69bafdf6b238e
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-05-24 14:43:00 -0400

    VER3-299 update repeated measure object

--- demoappback/model/repeated_measure.py
+++ demoappback/model/repeated_measure.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from demoappback.model.isu_factor import IsuFactor, IsuFactorType, IsuFactorNature
 
 
@@ -6,6 +8,10 @@ class RepeatedMeasure(IsuFactor):
     Class describing outcomes.
     """
 
-    def __init__(self, name: str = None):
+    def __init__(self, name: str = None, units: str = None, type: str = None, no_repeats: int = 2, partial_u_matrix: [] = [], correlation_matrix: [] = []):
         super.__init__(self, name, IsuFactorNature.WITHIN, IsuFactorType.REPEATED_MEASURE)
-        self.standard_deviation = None
+        self.units = ''
+        self.type = ''
+        self.no_repeats = 0
+        self.partial_u_matrix = np.matrix(partial_u_matrix)
+        self.correlation_matrix = np.matrix(correlation_matrix)
