ee568f90490e8ed176ad080f16b3c99131134fd4
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-05-29 10:48:14 -0400

    VER3-299 updae cluster object

--- demoappback/model/cluster.py
+++ demoappback/model/cluster.py
@@ -1,4 +1,4 @@
-from demoappback.model.isu_factor import IsuFactor, IsuFactorType, Nature
+from demoappback.model.isu_factor import IsuFactor, IsuFactorType, Nature, HypothesisType
 
 
 class Cluster(IsuFactor):
@@ -6,6 +6,19 @@ class Cluster(IsuFactor):
     Class describing clusters.
     """
 
-    def __init__(self, name: str = None, cluster_level: int = 0):
-        super().__init__(name, Nature.WITHIN, IsuFactorType.CLUSTER)
+    def __init__(self,
+                 name: str = None,
+                 values: [] = None,
+                 in_hypothesis: bool = False,
+                 child=None,
+                 partial_matrix=None,
+                 cluster_level: int = 0):
+        super().__init__(name=name,
+                         nature=Nature.WITHIN,
+                         factor_type=IsuFactorType.CLUSTER,
+                         values=values,
+                         in_hypothesis=in_hypothesis,
+                         hypothesis_type=HypothesisType.GLOBAL_TRENDS,
+                         child=child,
+                         partial_matrix=partial_matrix)
         self.cluster_level = cluster_level
