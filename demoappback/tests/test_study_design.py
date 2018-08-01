6f2f126060aa79a7d9fad1477fa80e97e3a107c8
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-08-01 15:20:49 -0400

    VER3-360 multiple outcomes loading

--- demoappback/tests/test_study_design.py
+++ demoappback/tests/test_study_design.py
@@ -50,7 +50,7 @@ class StudyDesignTestCase(unittest.TestCase):
         self.assertTrue(expected)
 
     def test_load_from_json(self):
-        """Should read the study design correctly from the model on model_1.json"""
+        """Should read the study design correctly from the model on model_2.json"""
         outcome_1 = Outcome(name='one')
         outcome_2 = Outcome(name='teo')
         rep_meas_1 = RepeatedMeasure(name='repMeas', values=[0, 1], units='time', type='Numeric', partial_u_matrix=np.matrix([[1],[-1]]), correlation_matrix=np.matrix([[1, 0],[0, 1]]))
@@ -63,7 +63,7 @@ class StudyDesignTestCase(unittest.TestCase):
                                  outcome_repeated_measure_st_devs=[
                                      OutcomeRepeatedMeasureStDev(outcome='one', repeated_measure='repMeas', values=[2, 3]),
                                      OutcomeRepeatedMeasureStDev(outcome='teo', repeated_measure='repMeas', values=[4, 5])])
-        
+
         expected = StudyDesign(isu_factors=isu_factors,
                                target_event=TargetEvent.REJECTION,
                                target_power=0.5,
@@ -87,10 +87,53 @@ class StudyDesignTestCase(unittest.TestCase):
         model.from_study_design(actual)
         power = unirep._chi_muller_muller_barton_1989(sigma_star=model.sigma_star,
                                                       rank_U=np.linalg.matrix_rank(model.u_matrix),
-                                                      total_N=20,
+                                                      total_N=model.total_n,
                                                       rank_X=np.linalg.matrix_rank(model.essence_design_matrix))
 
-        self.assertEqual(power, 0.96847792614988382)
+        self.assertEqual(power, 0.99365975196663026)
+
+    def test_load_multiple_outcomes(self):
+        """Should read the study design correctly from the model on model_3.json"""
+        outcome_1 = Outcome(name='one')
+        outcome_2 = Outcome(name='teo')
+        rep_meas_1 = RepeatedMeasure(name='repMeas', values=[0, 1], units='time', type='Numeric', partial_u_matrix=np.matrix([[1],[-1]]), correlation_matrix=np.matrix([[1, 0],[0, 1]]))
+        cluster_1 = Cluster(name='clstr', levels=[ClusterLevel(level_name='1'), ClusterLevel(level_name='2', no_elements=2)])
+        predictor_1 = Predictor(name='prdctr', values=['grp1', 'grp2'])
+
+        isu_factors = IsuFactors(variables=[outcome_1, outcome_2, rep_meas_1, cluster_1, predictor_1],
+                                 smallest_group_size=2,
+                                 outcome_correlation_matrix=np.matrix([[1, 0], [0, 1]]),
+                                 outcome_repeated_measure_st_devs=[
+                                     OutcomeRepeatedMeasureStDev(outcome='one', repeated_measure='repMeas', values=[2, 3]),
+                                     OutcomeRepeatedMeasureStDev(outcome='teo', repeated_measure='repMeas', values=[4, 5])])
+
+        expected = StudyDesign(isu_factors=isu_factors,
+                               target_event=TargetEvent.REJECTION,
+                               target_power=0.5,
+                               solve_for=SolveFor.POWER,
+                               confidence_interval_width=1,
+                               sample_size=10,
+                               selected_tests=[Tests.HOTELLING_LAWLEY, Tests.PILLAI_BARTLET, Tests.WILKS_LIKLIEHOOD],
+                               gaussian_covariate={'variance': 1},
+                               scale_factor=1,
+                               variance_scale_factor=[3, 4],
+                               power_curve=PowerCurve(confidence_interval=ConfidenceInterval(assumptions='Beta Fixed',
+                                                                                             beta_sample_size=10),
+                                                      x_axis='DesiredPower',
+                                                      data_series=[DataSeries(variance_scale_factor=3)]))
+
+        json_data = open("demoappback/tests/model_3.json")
+        data = json_data.read()
+        json_data.close()
+        actual = StudyDesign().load_from_json(data)
+        model = LinearModel()
+        model.from_study_design(actual)
+        power = unirep._chi_muller_muller_barton_1989(sigma_star=model.sigma_star,
+                                                      rank_U=np.linalg.matrix_rank(model.u_matrix),
+                                                      total_N=model.total_n,
+                                                      rank_X=np.linalg.matrix_rank(model.essence_design_matrix))
+
+        self.assertEqual(power, 1.0161495124112356)
 
 
 if __name__ == '__main__':
