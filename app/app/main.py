1d5d21a46a9296f2993d0d7b7b97053926e74e81
alasdair-macleod <alasdair.macleod@ufl.edu>
2023-02-03 10:00:04 +0000

    fix cert names - add sentry

--- app/app/main.py
+++ app/app/main.py
@@ -1,22 +1,29 @@
-
+import sentry_sdk
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
 from flask_pymongo import MongoClient
 from flask_cors import CORS
-
 from app.calculation_service import api
+
+sentry_sdk.init(
+    dsn="https://36775e9f3a3a43fbb055870c286b4a64@o4504611303260160.ingest.sentry.io/4504611305422848",
+    integrations=[
+        FlaskIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
+app = Flask(__name__)
 
 app = Flask(__name__)
 app.register_blueprint(api.bp)
 client = MongoClient('localhost', 27017)
 db = client['test-database']
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-
-
-@app.route('/')
-def hello_world():
-    """Hello world!"""
-    return 'Hello World!'
 
 
 if __name__ == '__main__':
